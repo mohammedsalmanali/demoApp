8370bdadf2c502733686b799f876810f61ffdaaf
Mohammed Salman Ali <46883461+mohammedsalmanali@users.noreply.github.com>
2022-12-31 19:03:01 +0000

    added imagea

--- demoApp/src/app/products/product-list.component.ts
+++ demoApp/src/app/products/product-list.component.ts
@@ -7,6 +7,9 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
   pageTitle = 'Product List';
+  imageWidth:number=50;
+  imageMargin:number=2;
+  showImage:boolean = false;
   products:any[] = [
     {
       "productId": 1,
@@ -29,4 +32,7 @@ export class ProductListComponent {
       "imageUrl": "assets/images/garden_cart.png"
     },
   ];
+  toggleImage():void {
+    this.showImage= !this.showImage;
+  }
 }
