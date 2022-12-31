c107dca5e34ad76ab6ed79fc2341a08d9cde722f
Mohammed Salman Ali <46883461+mohammedsalmanali@users.noreply.github.com>
2022-12-31 14:53:52 +0000

    create product list component

--- demoApp/src/app/app.module.ts
+++ demoApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { ProductListComponent } from './products/product-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
