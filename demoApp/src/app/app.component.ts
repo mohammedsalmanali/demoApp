845664b5a226b6a455ea083ede2326e6974ccf46
Mohammed Salman Ali <46883461+mohammedsalmanali@users.noreply.github.com>
2022-12-31 16:34:33 +0000

    moved html to separate file

--- demoApp/src/app/app.component.ts
+++ demoApp/src/app/app.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <div><h1>{{pageTitle}}</h1>
-    <app-product-list></app-product-list>
-    </div>
-    `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
